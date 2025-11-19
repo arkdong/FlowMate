d1d7e4e5991e3c968efa2fa0c0ab13e6c40c8ee9
Adam <ark.dong@outlook.com>
2025-11-19 15:00:21 +0100

    Initial Commit

---
+++ FlowMate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlowMate
+//
+//  Created by Adam on 19/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlowMate/FlowMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowMateApp.swift
+//  FlowMate
+//
+//  Created by Adam on 19/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct FlowMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
